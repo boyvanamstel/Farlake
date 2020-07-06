a085f41d4a17fabd1b7770d4ab399a60cff8fae5
Boy van Amstel <boy@boyvanamstel.nl>
2020-07-07 00:33:37 +0200

    Tweak gallery layout

--- Farlake/Shared/Features/Gallery/Utilities/UICollectionViewLayout+Gallery.swift
+++ Farlake/Shared/Features/Gallery/Utilities/UICollectionViewLayout+Gallery.swift
@@ -10,13 +10,15 @@ import UIKit
 
 extension UICollectionViewLayout {
     static var galleryGridLayout: UICollectionViewCompositionalLayout {
+        let columnCount: CGFloat = 3
+
         // Item
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0))
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0 / columnCount), heightDimension: .fractionalHeight(1.0))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = NSDirectionalEdgeInsets(equal: 5.0)
+        item.contentInsets = NSDirectionalEdgeInsets(equal: 1.0)
 
         // Group
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.5))
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(1.0 / columnCount))
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
 
         // Section

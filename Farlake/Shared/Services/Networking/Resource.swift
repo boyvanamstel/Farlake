84cb61d381762787b37b045fb4386bdf89a620a8
Boy van Amstel <boy@boyvanamstel.nl>
2020-07-07 10:55:35 +0200

    Move URLRequest helpers into separate file

--- Farlake/Shared/Services/Networking/Resource.swift
+++ Farlake/Shared/Services/Networking/Resource.swift
@@ -43,28 +43,3 @@ extension Resource where Object: UIImage {
 enum ResourceError: Error {
     case invalidRequest
 }
-
-typealias URLRequestParameters = [String: CustomStringConvertible]
-
-extension URLRequest {
-
-    /// Creates a new `URLRequest` with the parameters applied.
-    /// - Parameters:
-    ///   - url: The `URL` to apply parameters to.
-    ///   - parameters: The parameters to apply to the `url`.
-    init?(url: URL, parameters: URLRequestParameters) {
-        guard var components = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
-            return nil
-        }
-        components.queryItems = parameters.keys
-            .map { URLQueryItem(name: $0, value: parameters[$0]?.description) }
-            .sorted { $0.name < $1.name } // Sort to ensure caching works
-
-        guard let url = components.url else {
-            return nil
-        }
-
-        self.init(url: url)
-    }
-
-}
